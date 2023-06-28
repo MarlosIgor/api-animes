95f712e7bb8156148e4c4cb05eaa7da5689e9465
MarlosIgor <maroosigor@gmail.com>
2023-06-28 11:57:03 -0300

    teste1

--- src/main/java/br/com/api/animes/client/SpringClient.java
+++ src/main/java/br/com/api/animes/client/SpringClient.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 import java.util.List;
 
-// Fazer uma requisição para outros serviços ou URL externa
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
